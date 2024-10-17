e7b8b8cc79392e752cdb3216e87415908b63d5c8
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-10-17 23:33:08 +0530

    first commit express-app

---
+++ index.js
@@ -0,0 +1,68 @@
+import express from 'express';
+
+const app = express();
+
+const port = 8080;
+
+// app.get("/", (req,res) =>{
+//     res.send("Hello from server!!")
+// })
+// app.get("/ice-tea", (req,res) =>{
+//     res.send("Ice-tea found")
+// })
+// app.get("/name", (req,res) =>{
+//     res.send("Name found but not visible")
+// })
+
+app.use(express.json());
+
+let dataAll = [];
+let nextId = 1;
+
+app.post('/add-data',(req,res) => {
+    // destructing the json response
+    const {name, price} = req.body;
+    const newData = {id:nextId++, name, price};
+    dataAll.push(newData);
+    res.status(201).send(newData);
+})
+
+app.get('/all-data',(req,res) => {
+    res.status(200).send(dataAll);
+})
+
+// get by id
+app.get('/all-data/:id', (req, res) => {
+    // get each data named t and compare id of t with the id received through request 
+    const data = dataAll.find(t => t.id === parseInt(req.params.id));
+    if(!data){
+        return res.status(404).send("Data not found");
+    }
+    res.status(200).send(data);
+})
+
+// update
+app.put('/update/:id', (req,res) => {
+    const data = dataAll.find(t => t.id === parseInt(req.params.id));
+    if(!data){
+        return res.status(404).send("Data not found");
+    }
+    const {name, price} = req.body;
+    data.name = name;
+    data.price = price;
+    res.status(202).send(data)
+})
+
+// delete
+app.delete('/delete/:id',(req, res)=>{
+    const index = dataAll.findIndex(t => t.id === parseInt(req.params.id));
+    if(index === -1){
+        return res.status(404).send("Not found");
+    }
+    dataAll.splice(index, 1);
+    res.status(202).send("Successfuly deleted");
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running at port ${port}`)
+})
